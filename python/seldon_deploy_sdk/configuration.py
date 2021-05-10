0f4e9f3857e052be12d6351736d443b2af3d4ef1
ivan-valkov <iv.v.valkov@gmail.com>
2021-05-10 16:42:34 +0100

    Update version to 1.2.0

--- python/seldon_deploy_sdk/configuration.py
+++ python/seldon_deploy_sdk/configuration.py
@@ -252,5 +252,5 @@ class Configuration(object):
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: v1alpha1\n"\
-               "SDK Package Version: 0.2.1".\
+               "SDK Package Version: 1.2.0".\
                format(env=sys.platform, pyversion=sys.version)
