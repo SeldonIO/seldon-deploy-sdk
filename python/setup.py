82dc373efd2a355d1e056b333c8ee7955e43bdd4
SachinVarghese <smv@seldon.io>
2021-11-11 06:16:21 -0500

    Bumping version to 1.4.1

--- python/setup.py
+++ python/setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "seldon-deploy-sdk"
-VERSION = "1.4.0"
+VERSION = "1.4.1"
 # To install the library, run the following
 #
 # python setup.py install
